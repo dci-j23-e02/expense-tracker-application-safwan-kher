28be4990e5ee7a13dc4d8eb3b17d6929adf65b9b
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2024-06-11 12:03:09 +0200

    until user controller is updated

---
+++ src/main/java/com/example/expensetracker/controllers/ExpenseController.java
@@ -0,0 +1,5 @@
+package com.example.expensetracker.controllers;
+
+public class ExpenseController {
+
+}
---
+++ src/main/java/com/example/expensetracker/controllers/UserController.java
@@ -0,0 +1,42 @@
+package com.example.expensetracker.controllers;
+
+
+import com.example.expensetracker.models.User;
+import com.example.expensetracker.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+  @Autowired
+  private UserService userService;
+
+  @GetMapping("/login")
+  public String login(Model model){
+    model.addAttribute("errorMessage", "Invalid Credentials !!" );
+    return "login"; // Return login view
+  }
+
+
+  @GetMapping("/logout")
+  public String logout(){
+    return "redirect:/login?logout"; // Redirect to login page with logout parameter
+  }
+
+  @GetMapping("/signup")
+  public String showSignupForm(Model model){
+    model.addAttribute("user", new User());
+    return "signup";
+  }
+
+  @PostMapping("/signup")
+  public String signup(User user){
+    userService.saveUser(user);
+    return "redirect:/login";
+
+  }
+}

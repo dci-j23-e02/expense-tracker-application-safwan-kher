e8943c82bd7500a39fd396fc5393e9b994ec0a7c
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2024-06-12 16:06:30 +0200

    adding Security layer..

--- src/main/java/com/example/expensetracker/config/SecurityConfig.java
+++ src/main/java/com/example/expensetracker/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.expensetracker.config;
 
 import com.example.expensetracker.service.UserService;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +20,7 @@ public class SecurityConfig {
   public UserDetailsService userDetailsService(UserService userService){
     return userService;
   }
+
 
 
   @Bean

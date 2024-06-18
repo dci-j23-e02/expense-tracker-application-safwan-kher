d86b0defc0ac3fef09be75bbca8d3bddacf8aefe
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2024-06-18 12:33:45 +0200

    all upadates are added

--- src/main/java/com/example/expensetracker/config/MailConfig.java
+++ src/main/java/com/example/expensetracker/config/MailConfig.java
@@ -1,5 +1,6 @@
 package com.example.expensetracker.config;
 
+import java.util.Properties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,7 +11,19 @@ public class MailConfig {
 
   @Bean
   public JavaMailSender javaMailSender(){
-    JavaMailSender mailSender = new JavaMailSenderImpl();
+    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+    mailSender.setHost("smtp.gmail.com");
+    mailSender.setPort(587);
+    mailSender.setUsername("expensetracker51@gmail.com");
+    mailSender.setPassword("ijtq zaip vaac njtm");
+
+    Properties props = mailSender.getJavaMailProperties();
+    props.put("mail.transport.protocol", "smtp");
+    props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.starttls.enable", "true");
+    props.put("mail.debug", "true");
+
     return  mailSender;
     }
 
